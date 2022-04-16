d810db2c5ee0518acf1e443e9e0453169b4eea6b
Ademola101 <ademolaogunmokun@yahoo.com>
2022-04-16 19:09:38 +0100

    first atempt at conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
 import { useState } from 'react'
-const App = () => {
-  const [click, setClick] = useState({
-    left: 0,
-    right: 0
-  })
-
- const handleRightClick = () => {
-
-  setClick({ ...click,
-    right: click.right + 1})
-  };
-
-  const handleLeftClick = () => {
-       setClick({ ...click,
-        left: click.left + 1})
-
-  };
 
 
-  return(
-    <>
-      {click.left}
-      <button onClick = {handleLeftClick}> left </button>
-      <button onClick = {handleRightClick}> right </button>
-      {click.right}
-      
-      </>
-  )
+const History = ({allClick}) => {
+  if (allClick.length === 0) {
+    return (
+      <p>
 
+This app is used by pressing the button
+      </p>
+    )
+    }
+    return (
+     <p>
+
+button press history  {allClick.join(' ')}
+     </p>
+    )
   
 }
+const App = () => {
 
+const [right, setRight] = useState(0);
+const [left, setLeft] = useState(0);
+const [allClick, setAll] = useState([]);
+
+const handleRightClick = () => {
+  setAll(allClick.concat('R')) 
+  // concat because we dont want to modify the state directly
+  setRight(right + 1)
+};
+const handleLeftClick = () => {
+  setAll(allClick.concat('L'))
+  setLeft(left + 1)
+};
+
+// debugger
+
+return (
+  <> 
+  <button onClick={handleRightClick}>
+
+right
+  </button>
+  {right}
+  <button onClick={handleLeftClick}>
+
+left
+  </button>
+  {left}
+
+ <History allClick = {allClick} />
+  </>
+)
+
+}  
 export default App
