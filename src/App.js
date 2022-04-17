f812cef6e900bbe48d7d6845eb2c6fca1be2ce37
Ademola101 <ademolaogunmokun@yahoo.com>
2022-04-17 09:21:33 +0100

    first imple of stat

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 const Header = () => {
   return(
-    <p>
+    <h1>
 
       Give feedback
-    </p>
+    </h1>
   )
 };
 
@@ -23,18 +23,57 @@ const Section = () => {
     <section>
       Satistics
     </section>
+    
+
   )
 };
 
+const Stats = ({stat,text}) => {
+  return(
+<p>
+{text}: {stat}
+  
+</p>
+  )
+}
+
 const App = () => {
 
-  const [feedBack, setFeedBacks] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [feedback, SetFeedback] = useState([]);
+
+  const handleGoodClick = () => {
+    SetFeedback(feedback.concat("G"))
+    setGood(good + 1)
+  };
+
+  const handleBadClick = () => {
+    SetFeedback(feedback.concat("B"))
+    setBad(bad + 1)
+  }
+  const handleNeutralClick = () => {
+    SetFeedback(feedback.concat("N"))
+    setNeutral(neutral + 1)
+  }
+
+  const goodStat = () => {
+    return(
+    feedback.filter(item => item ==="G").length)
+  }
 
   return(
 <>
 <Header/>
+<Button onClick={handleGoodClick} text = "good"/>
+<Button onClick={handleNeutralClick} text = "neutral"/>
+<Button onClick={handleBadClick} text = "bad"/>
 
     <Section/>
+
+    <Stats stat = {goodStat} text = "good" />
+    <Stats text = "bad" stat = {goodStat}/>
 </>
 
     
